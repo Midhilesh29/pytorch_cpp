3929e88dae3095dc3a989d5ae792d2e295f050b8
Midhilesh29 <midhileshgithub@gmail.com>
2020-07-22 17:06:37 +0530

    added testing code

--- sample.cpp
+++ sample.cpp
@@ -45,13 +45,14 @@ int main(int argc, const char* argv[]) {
   try{
     module = torch::jit::load(argv[1]);
     //module.to(device);
+    /*
     auto test_dataset = torch::data::datasets::MNIST(
               kDataRoot, torch::data::datasets::MNIST::Mode::kTest)
               .map(torch::data::transforms::Normalize<>(0.1307, 0.3081))
               .map(torch::data::transforms::Stack<>());
     const size_t test_dataset_size = test_dataset.size().value();
     auto test_loader = torch::data::make_data_loader(std::move(test_dataset), kTestBatchSize);
-    test(module, device, *test_loader, test_dataset_size);
+    test(module, device, *test_loader, test_dataset_size);*/
   }
   catch (const c10::Error& e){
   	std::cerr << "error loading the model\n";
