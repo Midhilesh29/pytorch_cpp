6c4258536b8aef624290c6756bebfe43769f3f27
Midhilesh29 <midhileshgithub@gmail.com>
2020-07-29 12:30:16 +0530

    updated

--- sample.cpp
+++ sample.cpp
@@ -33,7 +33,16 @@ void test(torch::jit::script::Module model,torch::Device device, DataLoader& dat
 int main(int argc, const char* argv[]) {
   kDataRoot = argv[2];
   torch::manual_seed(1);
-  torch::DeviceType device_type = torch::kCPU;
+
+  torch::DeviceType device_type
+
+  if (torch::cuda::is_available()) {
+    std::cout << "CUDA available! Training on GPU." << std::endl;
+    device_type = torch::kCUDA;
+  } else {
+    std::cout << "Training on CPU." << std::endl;
+    device_type = torch::kCPU;
+  }
   torch::Device device(device_type);
   torch::jit::script::Module module;
   module = torch::jit::load(argv[1]);
